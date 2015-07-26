a6e442e4a7782433ebb8b45a601f8190e89ecec6
Stephan Kurpjuweit <stephan.kurpjuweit@gmail.com>
2015-07-26 18:26:36 +0200

    Initial commit

---
+++ 1_merge_data_sets.R
@@ -0,0 +1,46 @@
+# Script 1
+# file: 1_merge_data_sets.R
+# Merges the training and the test sets to create one data set
+# the result is stored in a data frame: result.df
+
+# Assumption: Data has been downloaded from https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+# and has been unzipped into the working directory
+
+
+# LOAD TEST DATA SET
+
+# Read fixed width format. Each line contains 561 variables with 15 characters each
+# Create a data frame containing the test data set
+width <- rep(16, 561)
+test.X.df <- read.fwf("UCI HAR Dataset/test/X_test.txt", width = width)
+test.df <- test.X.df
+
+# Read the file containing the activity label and add values in a new column "Activity.Label" to the test data frame
+test.Y.df <- read.table("UCI HAR Dataset/test/Y_test.txt")
+test.df$Activity.Label <- test.Y.df$V1
+
+# Read the file containing the subject identifier and add values in a new column "Subject.ID" to the test data frame
+subject.test.df <- read.table("UCI HAR Dataset/test/subject_test.txt")
+test.df$Subject.ID <- subject.test.df$V1
+
+
+# LOAD TRAIN DATA SET
+
+# Read fixed width format. Each line contains 561 variables with 15 characters each
+# Create a data frame containing the train data set
+train.X.df <- read.fwf("UCI HAR Dataset/train/X_train.txt", width = width)
+train.df <- train.X.df
+
+# Read the file containing the activity label and add values in a new column "Activity.Label" to the train data frame
+train.Y.df <- read.table("UCI HAR Dataset/train/Y_train.txt")
+train.df$Activity.Label <- train.Y.df$V1
+
+# Read the file containing the subject identifier and add values in a new column "Subject.ID" to the train data frame
+subject.train.df <- read.table("UCI HAR Dataset/train/subject_train.txt")
+train.df$Subject.ID <- subject.train.df$V1
+
+
+# CONCATENATE THE TEST AND THE TRAIN DATA SET
+
+result.df <- rbind(test.df, train.df)
+result.df
